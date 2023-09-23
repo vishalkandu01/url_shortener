dd42dc15ce5eeeb72faad5b6f83aadb3aa33f64f
Vishal Kandu <kanduvishal07@gmail.com>
2023-09-23 19:33:51 +0530

    schema is created and shortUrl thing is done in schema

---
+++ models/shortUrl.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const shortId = require('shortid')
+
+const shortUrlSchema = new mongoose.Schema({
+    full: {
+        type: String,
+        required: true
+    },
+    short: {
+        type: String,
+        required: true,
+        default: shortId.generate
+    }
+})
